36d92832dbf9e0f481c6b9c6eaefb37a56f802e0
tomassgi <149562014+tomassgi@users.noreply.github.com>
2024-01-17 10:12:56 +0200

    Update bank.py

--- bank.py
+++ bank.py
@@ -17,16 +17,30 @@ while True:
     choice = input("Enter your choice (1-4): ")
 
     if choice == '1':
+        choice_dep = int(input("Enter the amount you would like to deposit:"))
+        balance = balance + choice_dep
+        print("your balance is:" + str(balance))
+    
         # papildini kodu šeit
-        pass
+     
     elif choice == '2':
-        # papildini kodu šeit
-        pass
+        choice_with = int(input("Enter the amount you would like to withdraw:"))
+        if balance < choice_with:
+            print("You don't have sufficient funds")
+        else:
+            balance = balance - choice_with
+            print("your balance is:" + str(balance))
+
+        if balance < choice_with:
+            print("You don't have sufficient funds")
+            # papildini kodu šeit
+     
     elif choice == '3':
+        print("Your balance is" + str(balance))
         # papildini kodu šeit
         pass
     elif choice == '4':
         print("Exiting the banking system. Thank you!")
         break
     else:
-        print("Invalid choice. Please enter a number between 1 and 5.")
+        print("Invalid choice. Please enter a number between 1 and 4.")
